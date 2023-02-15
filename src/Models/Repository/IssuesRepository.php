5fe1cdd9b31c4715a340774ab67f92f5d5ec4947
Peter Dulacka <dulacka@gmail.com>
2023-02-15 10:44:35 +0100

    Add explicit types to the read-related Repository methods
    remp/crm#2761

--- src/Models/Repository/IssuesRepository.php
+++ src/Models/Repository/IssuesRepository.php
@@ -131,13 +131,13 @@ class IssuesRepository extends IssueBaseRepository
         ]);
     }
 
-    final public function totalCount($allowCached = false, $forceCacheUpdate = false)
+    final public function totalCount($allowCached = false, $forceCacheUpdate = false): int
     {
         $callable = function () {
             return parent::totalCount();
         };
         if ($allowCached) {
-            return $this->cacheRepository->loadAndUpdate(
+            return (int) $this->cacheRepository->loadAndUpdate(
                 'issues_count',
                 $callable,
                 \Nette\Utils\DateTime::from(CacheRepository::REFRESH_TIME_5_MINUTES),
